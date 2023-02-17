9b391e7ff09833288198b12d2ca895f1ebf1d883
Jeroen Heremans <44395687+JeroenHeremans@users.noreply.github.com>
2023-02-17 08:14:16 +0100

    Create YoutubeDownloader.py

---
+++ YoutubeDownloader.py
@@ -0,0 +1,30 @@
+from pytube import YouTube
+import sys
+
+
+def main():
+    link = input("Youtube Video URL: ")
+    title = input("Give a title: ")
+
+    youtubeDownloader = YouTube(link, on_progress_callback = progress_function)
+
+    print("Download in progress")
+
+    output_path = "C:\\Users\\Jeroen\\Downloads"
+    filename = f"{title}.mp4"
+    youtubeDownloader.streams.filter(progressive=True, file_extension="mp4").get_highest_resolution().download(output_path=output_path, filename=filename)
+
+    print("Video Downloaded", link)
+
+
+def progress_function(chunk, file_handle, bytes_remaining):
+    filesize = chunk.filesize
+    current = ((filesize - bytes_remaining)/filesize)
+    percent = ('{0:.1f}').format(current*100)
+    progress = int(50*current)
+    status = '█' * progress + '-' * (50 - progress)
+    sys.stdout.write(' ↳ |{bar}| {percent}%\r'.format(bar=status, percent=percent))
+    sys.stdout.flush()
+
+if __name__=="__main__":
+    main()
